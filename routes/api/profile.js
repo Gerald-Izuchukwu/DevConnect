f1070d94f95943cb8e4b0c69cc5347e64c3c1fd1
GERALD IZUCHUKWU <39552823+Gerald-Izuchukwu@users.noreply.github.com>
2021-07-19 11:38:21 +0100

    Getting all profiles and getting profiles by user id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -92,7 +92,44 @@ router.post('/', [auth, [
             
         }
 
-        res.send('Hello')
     }
 )
+
+// @route    GET api/profile/
+// @desc     Get all profile
+// @access   Public
+
+router.get('/', async(req, res)=>{
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json(profiles)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server Error')
+        
+    }
+})
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user id
+// @access   Public
+
+router.get('/user/:user_id', async(req, res)=>{
+    const { user_id } = req.params
+    try {
+        const profile = await Profile.findOne({user : user_id }).populate('user', ['name', 'avatar'])
+        if(!profile){
+            return res.status(400).json({msg : 'there is no profile for this user'})
+        }
+        res.json(profile)
+    } catch (error) {
+        console.error(error.message)
+        if(error.kind == "ObjectId"){
+            return res.status(400).json({msg : 'there is no profile for this user'})
+        }
+        res.status(500).send('Server Error')
+        
+    }
+})
+
 module.exports = router
