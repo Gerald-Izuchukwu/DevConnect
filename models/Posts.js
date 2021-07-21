a0e9b3b38abeebc95be772ad1385625907af7887
GERALD IZUCHUKWU <39552823+Gerald-Izuchukwu@users.noreply.github.com>
2021-07-21 10:41:00 +0100

    Created post model and add post route

--- models/Posts.js
+++ models/Posts.js
@@ -5,5 +5,49 @@ const PostSchema = Schema({
     user : {
         type : Schema.Types.ObjectId,
         ref : 'User'
+    },
+    text : {
+        type : String,
+        required : true
+    },
+    name : {
+        type : String
+    },
+    avatar : {
+        type : String
+    },
+    likes : [
+        {
+            user : {
+                type : Schema.Types.ObjectId,
+                ref : 'Users'
+            }
+        }
+    ],
+    comment : [
+        {
+            user : {
+                type : Schema.Types.ObjectId,
+                ref : 'Users'
+            },
+            text : {
+                type : String,
+                required : true
+            },
+            avatar :{
+                type : String
+            },
+            date : {
+                type : Date,
+                default : Date.now
+            }
+        }
+    ],
+
+    date : {
+        type : Date,
+        default : Date.now
     }
 })
+
+module.exports = Post = mongoose.model('Post', PostSchema)
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const {check, validationResult} = require('express-validator');
+const User = require('../../models/Users');
+const Profile = require('../../models/Profile');
+const Posts = require('../../models/Posts');
 
 
 // @route    POST api/posts
-// @desc     Register user
-// @access   Public
-router.get('/', (req, res)=> res.send('Posts Route'));
+// @desc     Create a post
+// @access   Private
+router.post('/', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async(req, res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()})
+    }
+    
+
+
+    try {
+        const  { id } = req.user
+        const user = await User.findById(id).select('-password')
+
+        const newPost = new Posts({
+            text : req.body.text,
+            name : user.name,
+            avatar : user.avatar,
+            user : id
+        })
+
+        const post = await newPost.save()
+
+        res.json(post)
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+        
+    }
+
+});
+
 
 module.exports = router
